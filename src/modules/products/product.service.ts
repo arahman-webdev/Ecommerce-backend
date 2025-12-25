c2bafb7268a33bcf3e6542c57feb9d2e86c1d4f4
Abdur Rahman <upworkabdurrahman@gmail.com>
2025-12-25 10:13:07 +0600

    ssl payment service

--- src/modules/products/product.service.ts
+++ src/modules/products/product.service.ts
@@ -36,7 +36,7 @@ const getCategory = async () => {
       include:{
         products:{
           select:{
-            productImages:true
+            productImages:true 
           }
         }
       }
